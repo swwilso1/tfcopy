8d9b5927b21c37f6257345f4e44e14503501dc68
Steve Wilson <swwilso1@protonmail.com>
2023-02-20 19:25:08 +0000

    Update the version number to 1.0.1.

--- src/data_model.hpp
+++ src/data_model.hpp
@@ -50,7 +50,7 @@ namespace copy
         using version_type = Version;
 
         string_type tool_name{"Tectiform Visual Copy Tool"};
-        version_type tool_version{1, 0, 0};
+        version_type tool_version{1, 0, 1};
 
         ButtonOption button_style{ButtonOption::Simple()};
         Color background_color{Color::NavyBlue};
